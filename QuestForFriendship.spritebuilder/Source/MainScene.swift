01146ab3217e99269827592019660589320f9da7
sheriffsoco <japenes@gmail.com>
2015-03-08 22:34:39 -0700

    test commit

--- QuestForFriendship.spritebuilder/Source/MainScene.swift
+++ QuestForFriendship.spritebuilder/Source/MainScene.swift
@@ -1,5 +1,28 @@
 import Foundation
 
 class MainScene: CCNode {
+    var movementSpeed : CGFloat = 2
+    var connor : CCSprite!
+    
+    func didLoadFromCCB() {
+        self.userInteractionEnabled = true
+    }
+    
+    override func update(delta: CCTime) {
+        
+    }
+    
+    override func touchBegan(touch: CCTouch!, withEvent: CCTouchEvent!) {
+        var location = touch.locationInNode(self)
+        
+        if location.x < 284 {
+            connor.position = ccp(connor.position.x - movementSpeed, connor.position.y)
+            println(connor.position.x)
+        }
+        else {
+            connor.position = ccp(connor.position.x + movementSpeed, connor.position.y)
+            println(connor.position.x)
+        }
+    }
 
 }
