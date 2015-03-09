14e6b6d03295c8f6ca1f9903ecf099dbac1a49e4
sheriffsoco <japenes@gmail.com>
2015-03-08 23:08:13 -0700

    Implement Movement
    Includes:
    - fluid motion in one click
    - sprite flipping

--- QuestForFriendship.spritebuilder/Source/MainScene.swift
+++ QuestForFriendship.spritebuilder/Source/MainScene.swift
@@ -2,6 +2,7 @@ import Foundation
 
 class MainScene: CCNode {
     var movementSpeed : CGFloat = 2
+    var movement : CGFloat = 0
     var connor : CCSprite!
     
     func didLoadFromCCB() {
@@ -9,20 +10,29 @@ class MainScene: CCNode {
     }
     
     override func update(delta: CCTime) {
-        
+        if movement == -1 {
+            connor.position = ccp(connor.position.x - movementSpeed, connor.position.y)
+            connor.flipX = true
+        }
+        else if movement == 1 {
+            connor.position = ccp(connor.position.x + movementSpeed, connor.position.y)
+            connor.flipX = false
+        }
     }
     
     override func touchBegan(touch: CCTouch!, withEvent: CCTouchEvent!) {
         var location = touch.locationInNode(self)
         
         if location.x < 284 {
-            connor.position = ccp(connor.position.x - movementSpeed, connor.position.y)
-            println(connor.position.x)
+            movement = -1
         }
         else {
-            connor.position = ccp(connor.position.x + movementSpeed, connor.position.y)
-            println(connor.position.x)
+            movement = 1
         }
+    }
+
+    override func touchEnded(touch: CCTouch!, withEvent: CCTouchEvent!) {
+        movement = 0
     }
 
 }
