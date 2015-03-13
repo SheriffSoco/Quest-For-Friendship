e9753c299fae7dda14e1e11ef46d287932e98325
sheriffsoco <japenes@gmail.com>
2015-03-13 11:28:23 -0700

    Add house
    - Need to create house entering function

---
+++ QuestForFriendship.spritebuilder/House.swift
@@ -0,0 +1,18 @@
+//
+//  House.swift
+//  QuestForFriendship
+//
+//  Created by Jacob Apenes on 3/13/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+import Foundation
+
+class House : CCSprite {
+    var friendList : [CGFloat] = []
+    var backgroundImage : CGFloat = -1
+    var groundImage : CGFloat = -1
+    var xDoorLocation : CGFloat = 0
+    var yDoorLocation : CGFloat = 0
+    
+}
